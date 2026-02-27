b8da02ef2d01d5b3e258c433f905d64538d92d6a
Zong_Xina <emmanreyes114@gmail.com>
2026-02-27 10:01:00 +0800

    Added some Front-end features

--- PHP/index.php
+++ PHP/index.php
@@ -1,3 +1,18 @@
 <?php
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <img src="RES/IMG/chidog.png" alt="dog">
+    <img src="" alt="">
+    <img src="" alt="">
+    <img src="" alt="">
+</body>
+</html>
